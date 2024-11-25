3e1a50b5fa0158fe46a690d05a6906fc21648a21
xStarRier <stephanniequynh@gmail.com>
2024-11-25 13:45:03 +0100

    Add project files.

---
+++ ElseIf DEMO/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace IfElsetest
+{
+
+internal class ifElse
+{
+    int number1 = 5;
+    int number2 = 1;
+    bool isEqual = false;
+
+    public void Run()
+    { 
+        if(number1 == number2 )
+        {
+            isEqual = true;
+            Console.WriteLine("The numbers are equal");
+        }
+        else
+        {
+            isEqual = false;
+            Console.WriteLine("The numbers are not equal");
+        }
+
+        if (isEqual)
+        {
+            number1++;
+        }
+        else
+        {
+            number2--;
+        }
+
+
+    }
+
+
+
+}
+
+}
